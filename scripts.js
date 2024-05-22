b19d4e215ef02e34a886ec0204989641fc74f294
CreativeCornerGiftShop <creativecornerbygaur@gmail.com>
2024-05-22 12:24:21 +0100

    Added add-to-cart button functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Add your JavaScript code here
+    const cartButtons = document.querySelectorAll('.product button');
+    
+    cartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Item added to cart!');
+        });
+    });
 });
